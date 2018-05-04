d8834cb56d448f4011e60d97cf1ed12ba4be8b77
Nathan King <nathanking3590@gmail.com>
2018-05-04 11:01:00 +0100

    Refactor test lambda

--- src/test/java/command/HandlerResolverTest.java
+++ src/test/java/command/HandlerResolverTest.java
@@ -15,8 +15,6 @@ class HandlerResolverTest {
 
     @org.junit.jupiter.api.Test
     void resolveHandlerThrowsException() throws HandlerResolutionException {
-        assertThrows(IncompatibleCommandException.class, () -> {
-            HandlerResolver.resolveHandler(new TestCommand("testname")).handle(new InvalidTestCommand());
-        });
+        assertThrows(IncompatibleCommandException.class, () -> HandlerResolver.resolveHandler(new TestCommand("testname")).handle(new InvalidTestCommand()));
     }
 }

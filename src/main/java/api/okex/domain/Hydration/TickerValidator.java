ef344166c48fec518d0afa908cdb77ba59e0ff4c
Nathan King <nathanking3590@gmail.com>
2018-05-07 12:21:14 +0100

    Add ticker hydration validator

---
+++ src/main/java/api/okex/domain/Hydration/TickerValidator.java
@@ -0,0 +1,26 @@
+package api.okex.domain.Hydration;
+
+import api.okex.domain.Hydration.exception.HydrationException;
+import com.google.gson.JsonObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class TickerValidator {
+    public void validate(JsonObject jsonObject) throws HydrationException {
+        ArrayList<String> required = new ArrayList<String>(Arrays.asList(
+                "buy",
+                "high",
+                "last",
+                "low",
+                "sell",
+                "vol"
+        ));
+
+        for (String field : required) {
+            if (!jsonObject.has(field)) {
+                throw new HydrationException("JSON entity missing required field: " + field);
+            }
+        }
+    }
+}

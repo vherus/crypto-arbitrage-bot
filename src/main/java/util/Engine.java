343e055df77baf8602efa524adb1c1c9a27381ab
Nathan King <nathanking3590@gmail.com>
2018-05-29 14:21:24 +0100

    Add basic test logic

--- src/main/java/util/Engine.java
+++ src/main/java/util/Engine.java
@@ -8,7 +8,9 @@ import org.knowm.xchange.currency.CurrencyPair;
 import org.knowm.xchange.dto.marketdata.OrderBook;
 import org.knowm.xchange.dto.trade.LimitOrder;
 import javax.inject.Inject;
-import java.util.ArrayList;
+import java.math.BigDecimal;
+import java.util.Collections;
+import java.util.List;
 
 final public class Engine {
     final private CommandBus commandBus;
@@ -19,9 +21,33 @@ final public class Engine {
     }
 
     public void run(Integer transactionPercentage) throws IncompatibleCommandException, HandlerResolutionException, CommandExecutionException {
-        ArrayList<LimitOrder> orderBook = OrderBookMerger.merge(
-            ((OrderBook) commandBus.dispatch(new okex.command.GetOrderBookCommand(CurrencyPair.LTC_BTC))).getBids(),
-            ((OrderBook) commandBus.dispatch(new bittrex.command.GetOrderBookCommand(CurrencyPair.LTC_BTC))).getBids()
+        List<LimitOrder> okexBook = ((OrderBook) commandBus.dispatch(new okex.command.GetOrderBookCommand(CurrencyPair.LTC_BTC))).getBids();
+        List<LimitOrder> bittrexBook = ((OrderBook) commandBus.dispatch(new bittrex.command.GetOrderBookCommand(CurrencyPair.LTC_BTC))).getBids();
+//        ArrayList<LimitOrder> mergedBook = OrderBookMerger.merge(okexBook, bittrexBook);
+
+        // Default order books are ordered lowest price to highest
+        Collections.reverse(okexBook);
+        Collections.reverse(bittrexBook);
+
+        LimitOrder bittrexTop = bittrexBook.get(0);
+        LimitOrder okexTop = okexBook.get(0);
+
+        BigDecimal fee = new BigDecimal(1 - (100 / transactionPercentage));
+
+        String okexOrderId = (String) commandBus.dispatch(
+                new okex.command.PlaceBuyOrderCommand(CurrencyPair.LTC_BTC, bittrexTop.getLimitPrice().multiply(fee), bittrexTop.getOriginalAmount())
         );
+
+        String bittrexOrderId = (String) commandBus.dispatch(
+                new bittrex.command.PlaceBuyOrderCommand(CurrencyPair.LTC_BTC, okexTop.getLimitPrice().multiply(fee), okexTop.getOriginalAmount())
+        );
+
+        // todo Check the status of each order every n seconds
+
+        // if okex order is fulfilled...
+        commandBus.dispatch(new bittrex.command.PlaceSellOrderCommand(CurrencyPair.LTC_BTC, bittrexTop.getLimitPrice(), bittrexTop.getOriginalAmount()));
+
+        // if bittrex order is fulfilled...
+        commandBus.dispatch(new okex.command.PlaceSellOrderCommand(CurrencyPair.LTC_BTC, okexTop.getLimitPrice(), okexTop.getOriginalAmount()));
     }
 }

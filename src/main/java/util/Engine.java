7bc32e5599d9973634fef7c55f0cb5d4542ddd42
Nathan King <nathanking3590@gmail.com>
2018-05-29 13:02:00 +0100

    Use merger

--- src/main/java/util/Engine.java
+++ src/main/java/util/Engine.java
@@ -1,8 +1,14 @@
 package util;
 
 import command.CommandBus;
-
+import command.exception.CommandExecutionException;
+import command.exception.HandlerResolutionException;
+import command.exception.IncompatibleCommandException;
+import org.knowm.xchange.currency.CurrencyPair;
+import org.knowm.xchange.dto.marketdata.OrderBook;
+import org.knowm.xchange.dto.trade.LimitOrder;
 import javax.inject.Inject;
+import java.util.ArrayList;
 
 final public class Engine {
     final private CommandBus commandBus;
@@ -10,5 +16,12 @@ final public class Engine {
     @Inject
     public Engine(CommandBus commandBus) {
         this.commandBus = commandBus;
+    }
+
+    public void run() throws IncompatibleCommandException, HandlerResolutionException, CommandExecutionException {
+        ArrayList<LimitOrder> orderBook = OrderBookMerger.merge(
+            ((OrderBook) commandBus.dispatch(new okex.command.GetOrderBookCommand(CurrencyPair.LTC_BTC))).getAsks(),
+            ((OrderBook) commandBus.dispatch(new bittrex.command.GetOrderBookCommand(CurrencyPair.LTC_BTC))).getAsks()
+        );
     }
 }
